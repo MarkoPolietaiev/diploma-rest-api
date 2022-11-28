6da6ae409d07a051ebd60431a891ae8105175db5
Marko Polietaiev <markopolietaiev@gmail.com>
2022-11-27 18:13:04 -0800

    Added searching to user profile API

--- diploma_api/views.py
+++ diploma_api/views.py
@@ -6,6 +6,7 @@ from rest_framework import viewsets
 from diploma_api import models
 from rest_framework.authentication import TokenAuthentication
 from diploma_api import permissions
+from rest_framework import filters
 
 
 class HelloApiView(APIView):
@@ -101,4 +102,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
     
